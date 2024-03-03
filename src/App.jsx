086530c3afe7afa387916da1c30ecf9a6a1015a9
Dor Katzir <katzirdor@gmail.com>
2024-03-03 23:48:24 +0200

    css styles and error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { useState } from 'react'
 import './app.css'
-import FormInput from "./components/FormInput";
+import FormInput from "./components/FormInput"
 
 export default function App() {
 
@@ -12,43 +13,56 @@ export default function App() {
     confirmPassword: ''
   })
 
-  const inputs = [
-		{
-			id: 1,
-			name: 'username',
-			type: 'text',
-			placeholder: 'Username',
-			lable: 'Username',
-		},
-		{
-			id: 2,
-			name: 'email',
-			type: 'text',
-			placeholder: 'Email',
-			lable: 'Email',
-		},
-		{
-			id: 3,
-			name: 'birthday',
-			type: 'text',
-			placeholder: 'Birthday',
-			lable: 'Birthday',
-		},
-		{
-			id: 4,
-			name: 'password',
-			type: 'password',
-			placeholder: 'Password',
-			lable: 'Password',
-		},
-		{
-			id: 5,
-			name: 'confirmPassword',
-			type: 'password',
-			placeholder: 'Confirm Password',
-			lable: 'Confirm Password',
-		},
-  ]
+const inputs = [
+	{
+		id: 1,
+		name: 'username',
+		type: 'text',
+		placeholder: 'Username',
+		errorMessage:
+			"Username should be 3-16 characters and shouldn't include any special character!",
+		label: 'Username',
+		pattern: '^[A-Za-z0-9]{3,16}$',
+		required: true,
+	},
+	{
+		id: 2,
+		name: 'email',
+		type: 'email',
+		placeholder: 'Email',
+		errorMessage: 'It should be a valid email address!',
+		label: 'Email',
+		required: true,
+	},
+	{
+		id: 3,
+		name: 'birthday',
+		type: 'date',
+		placeholder: 'Birthday',
+		label: 'Birthday',
+	},
+	{
+		id: 4,
+		name: 'password',
+		type: 'password',
+		placeholder: 'Password',
+		errorMessage:
+			'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
+		label: 'Password',
+		pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+		required: true,
+	},
+	{
+		id: 5,
+		name: 'confirmPassword',
+		type: 'password',
+		placeholder: 'Confirm Password',
+		errorMessage: "Passwords don't match!",
+		label: 'Confirm Password',
+		pattern: values.password,
+		required: true,
+	},
+]
 
   console.log('re-rendered')
 
@@ -59,23 +73,23 @@ export default function App() {
 
   const onChange = (e)=>{
     setValues({...values, [e.target.name]: e.target.value})
-    
   }
 
   console.log(values)
   return (
 		<div className='app'>
 			<form onSubmit={handleSubmit}>
-        {
-          inputs.map(input => <FormInput 
-                                  key={input.id}
-                                  {...input}
-                                  value={values[input.name]}
-                                  onChange={onChange}
-                                />)
-        }
+				<h1>Register</h1>
+				{
+					inputs.map(input => <FormInput 
+											key={input.id}
+											{...input}
+											value={values[input.name]}
+											onChange={onChange}
+											/>)
+				}
 				
-        <button>Submit</button>
+        		<button>Submit</button>
 			</form>
 		</div>
   )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
 import './formInput.css'
 
 export default function FormInput(props) {
-    const {label, onChange, id, ...inputProps} = props
+    const { lable, errorMessage, onChange, id, ...inputProps } = props
 	return (
 		<div className='formInput'>
-			<label>{label}</label>
+			<label>{lable}</label>
 			<input {...inputProps} onChange={onChange} />
+			<span>{errorMessage}</span>
 		</div>
 	)
 }
