bde1adbed6e0fb929732579908975ad124797a05
Dor Katzir <katzirdor@gmail.com>
2024-03-04 11:27:30 +0200

    username capitalize and label type fixed

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,11 +4,11 @@
 import './formInput.css'
 
 export default function FormInput(props) {
-    const { lable, errorMessage, onChange, id, ...inputProps } = props
+    const { label, errorMessage, onChange, id, ...inputProps } = props
 	return (
 		<div className='formInput'>
-			<label>{lable}</label>
-			<input {...inputProps} onChange={onChange} />
+			<label>{label}</label>
+			<input className={label==='Username'&&'firstLetter'} {...inputProps} onChange={onChange} />
 			<span>{errorMessage}</span>
 		</div>
 	)
